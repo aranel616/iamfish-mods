b32782bf97b7ea79633e1235634c9f38b3a04d68
Sarah Day <aranel616@gmail.com>
2022-07-30 21:35:14 -0700

    Adding God Mode

---
+++ GodMode/Class1.cs
@@ -0,0 +1,54 @@
+﻿using UnityModManagerNet;
+using HarmonyLib;
+using System.Reflection;
+using IAmFish.Fish;
+using UnityEngine;
+
+namespace GodMode {
+    static class Main {
+        public static bool enabled;
+        public static bool godMode = false;
+        
+        public static UnityModManager.ModEntry mod;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            var harmony = new Harmony(modEntry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            mod = modEntry;
+            modEntry.OnToggle = OnToggle;
+
+            return true;
+        }
+
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            enabled = value;
+
+            return true;
+        }
+
+        public static void ToggleGodMode()
+        {
+            godMode = !godMode;
+        }
+    }
+
+    [HarmonyPatch(typeof(FishSuffocation), "Update")]
+    static class FishSuffocationUpdatePatch {
+        static bool Prefix()
+        {
+            if (!Main.enabled)
+                return true;
+
+            if (Input.GetKeyDown(KeyCode.F1))
+                Main.ToggleGodMode();
+
+            if (Main.godMode)
+                return false;
+
+            return true;
+        }
+    }
+}
